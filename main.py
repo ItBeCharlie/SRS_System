441c104889e89c077542f401a64b4fa1b8b327f8
ItBeCharlie <charlied713@gmail.com>
2025-02-03 00:11:44 -0500

    Started work on implementing KMP

--- main.py
+++ main.py
@@ -1,18 +1,21 @@
 from SRS import SRS
 from PMM import PMM
+from Helper import KMPSearch
 import sys
 
 
 def main():
-    srs_path = sys.argv[1]
-    srs = SRS(srs_path)
+    # srs_path = sys.argv[1]
+    # srs = SRS(srs_path)
 
-    srs.print_terms()
+    # srs.print_terms()
 
-    srs.PMM.print_transition_table()
-    srs.PMM.print_failure_function()
-    srs.PMM.print_output_function()
-    # basic_pmm_test()
+    # srs.PMM.print_transition_table()
+    # srs.PMM.print_failure_function()
+    # srs.PMM.print_output_function()
+
+    # KMPSearch("abba", "babbababbbabbabb")
+    basic_pmm_test()
 
 
 def basic_pmm_test():
@@ -24,11 +27,6 @@ def basic_pmm_test():
     test_pmm.print_output_function()
     matches = test_pmm.find_match("ushers")
     print(matches)
-
-    pmm2 = PMM(["aba", "bb"], "ab")
-    pmm2.print_transition_table()
-    pmm2.print_failure_function()
-    pmm2.print_output_function()
 
 
 main()
